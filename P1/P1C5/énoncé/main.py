f00f85a25de3ca22192dd457c6b2697279f5bdfa
Dimerel <dimerel.ndza@gmail.com>
2025-05-17 23:15:08 +0100

    Update P1C5 main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+nom= "NDZA-OFANY"
+age= 28
+taille= 1.70
+est_etudiant= True
+print("Nom :" + nom + "\nAge: " +str(age)+ "\nTaille: "+str(taille)+"\nEst étudiant: "+ str(est_etudiant))
+
+print("Type nom:") 
+print(type(nom))
+print("Type age:") 
+print(type(age))
+print("Type taille:") 
+print(type(taille))
+print("Type est étudiant:") 
+print(type(est_etudiant))
